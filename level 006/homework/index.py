fdbf0d5ecbe54aaf51077d3bde10ef0b356c603b
lukazaqaidze <monkeypro933@gmail.com>
2026-05-30 16:14:14 +0400

    second commit

---
+++ level 006/homework/index.py
@@ -0,0 +1,72 @@
+# 1)
+# num1 = int(input("enter your first numer: "))
+# num2 = int(input("enter your sechond number: "))
+# num3 = int(input("enter your third number: "))
+
+# average = (num1 + num2 + num3) / 3
+
+# print("საშვლო არითმეტიკულია:", average)
+
+# 2)
+# num1 = int(input("enter your first numer: "))
+# num2 = int(input("enter your sechond number: "))
+# print(num1 + num2)
+
+# 3)
+# Data Type ნიშნავს მონაცემის ტიპს.
+# Python-ში ყველა მონაცემს თავისი ტიპი აქვს.
+
+# int 
+
+# num = 10
+
+# int არის მთელი რიცხვი.
+# მაგალითად: 1, 5, 100, -7
+
+# Python int ტიპს ჩვეულებრივ რიცხვად აღიქვამს
+# და შეუძლია მათემატიკური მოქმედებების შესრულება.
+
+
+
+# float 
+
+# height = 1.75
+
+# float არის ათწილადი რიცხვი.
+# მაგალითად: 2.5, 7.9, -1.3
+
+# Python float-ს იყენებს მაშინ,
+# როცა რიცხვს წერტილი აქვს.
+
+# str
+
+# name = "lukashark"
+
+# str არის ტექსტი.
+# ტექსტი იწერება ბრჭყალებში.
+
+# Python string-ს ტექსტად აღიქვამს
+# და არა რიცხვად.
+
+
+
+# bool ნიშნავს True ან False-ს.
+
+# Python ამ ტიპს იყენებს
+# პირობებში და შედარებებში.
+
+
+# int -> მთელი რიცხვი
+# float -> ათწილადი
+# str -> ტექსტი
+# bool -> True / False
+
+# 4)
+# myname="luka"
+# sistername="nini"
+# sistername2="anamaria"
+# mothername="eka"
+# fathername="gocha"
+
+
+# print("my name is " + myname + ", my  sisters names are " + sistername + "and " + sistername2 + ", my mothers name is " + mothername + ", and lastly my fathers name is" + fathername)
---
+++ level 008/homework/index.py
@@ -0,0 +1,58 @@
+# 1)
+# input es aris funqcia ritic cven segvaqvs informacia kompiutersi
+#output es aris is informacia romelsac kompiuteri gamogvitans
+
+#2)
+# name = type(input("enter your name"))
+
+#3)
+
+#int
+age1=69
+num=67
+year=1  
+month=2
+building=4
+
+#str
+name="luka"
+month="minecraft"
+box="toybox"
+country="georgia"
+planet="planetlord"
+
+#float
+age=69.67
+num=67.61
+year=1.0
+month=2.5
+building=4.6
+
+#4)
+type(age1)
+type(name)
+type(building)
+
+# 5)
+gg=input("enter your word: ")
+xd=input("enter your word: ")
+print(gg+xd)
+
+# 6
+go=int(input("enter your number: "))
+xo=int(input("enter your number: "))
+gs=int(input("enter your number: "))
+xs=int(input("enter your number: "))
+gc=int(input("enter your number: "))
+print((go+xo+gs+xs+gc)/5)
+
+# 7)
+myname=input("enter name")
+sistername=input("enter name")
+sistername2=input("enter name")
+mothername=input("enter name")
+fathername=input("enter name")
+
+
+
+print("my name is " + myname + ", my  sisters names are " + sistername + "and " + sistername2 + ", my mothers name is " + mothername + ", and lastly my fathers name is" + fathername)
