a990e20106d893f324107c9976d74e37751cfb7b
lukazaqaidze <monkeypro933@gmail.com>
2026-07-03 18:19:17 +0400

    second commit

---
+++ level 006/classwork/index.py
@@ -0,0 +1,41 @@
+# Level 006
+# 1) შექმენი ერთი ცვლადი სადაც შემოიტან შენს ასაკს, შემდეგ დაპრინტე შედეგი
+
+# 2) კომენტარის სახით ახსენი რა არის input და რა არის output და ასევე ახსენი განსხვავება მათ შორის
+
+
+# 3) შექმენი ორი ცვლადი, სადაც შემოიტან ორ რიცხვს, შემდეგ ამ რიცხვებს შორის მოახდინე: მიმატება, გამოკლება, გამრავლება და გაყოფა
+
+
+# 4) მომხმარებელს შემოატანინე თავის საყვარელი ფერი, ცხოველი და დასასვენებელი კურორტი ან ქვეყანა, შემდეგ დაპრინე წინადადება მაგალითად: "Your favorites are: Color: " ლურჯი ", Animal: " + ძაღლი + ", Place: " + ეიფელის კოშკი)
+
+# 5) input ად შემოიტანე 4 მანქანის სახელი და მოდელი შემდეგ დაპრინტე ისინი შენი გემოვნებით, ანუ პირველი დაპრინტე იმ მანქანის სახელი რომელიც ყველაზე მეტად მოგწონს მაგ ინფუთიდან
+
+
+num = 15
+print(num)
+
+# input aris informaciis setana output ki gamotana
+
+num1 = 1
+num2 = 2
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1//num2)
+
+fav_color=input("enter your favourite color:")
+pet=input("enter your favourite pet:")
+country =input("enter your favourite country:")
+
+print("your favourite color pet and countries are "+ fav_color + pet + country)
+
+
+car1=input("enter your cars name:")
+car2=input("enter your cars name:")
+car3=input("enter your cars name:")
+car4=input("enter your cars name:")
+print(car1)
+print(car2)
+print(car3)
+print(car4)
